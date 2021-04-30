0662faf9f8f22e43ab45fb7f4b7bff5270c28827
Amor Osmic <amorosmic@gmx.com>
2021-04-30 14:29:00 +0200

    Created words controller

---
+++ Controllers/WordsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Skraebul_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordsController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Afrika paprika"
+        };
+
+        private readonly ILogger<WordsController> _logger;
+
+        public WordsController(ILogger<WordsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new {
+                word = Summaries[new Random().Next(0, Summaries.Length)]
+            });
+        }
+    }
+}
