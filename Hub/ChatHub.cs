449b12b4308727fd0b8a6c05e14e2125c83b5b59
Amor Osmic <amorosmic@gmx.com>
2021-05-11 14:15:26 +0200

    Caches users inside a gamer room and shows who is playing and who left

--- Hub/ChatHub.cs
+++ Hub/ChatHub.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
@@ -5,9 +8,44 @@ namespace Hubs
 {
     public class ChatHub : Hub
     {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
         public async Task SendMessage(string username, string message)
         {
             await Clients.All.SendAsync("RecieveMessage", new {username = username, message = message});
+        }
+
+        public override async Task OnConnectedAsync() 
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Test");
+
+            string users = "";
+            if (Users.ContainsKey("Test"))
+            {
+                users = Users["Test"];
+            }
+
+            var username = Context.GetHttpContext().Request.Query["username"].ToString();
+
+            if (username == "")
+            {
+                Context.Abort();
+            }
+
+            Context.Items.Add("Username", username);
+            users += username + "|";
+            Users["Test"] = users;
+
+            await Clients.Group("Test").SendAsync("Connected", users, username);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception) 
+        {
+            string username = Context.Items["Username"].ToString();
+            Users["Test"] = Users["Test"].Replace(username + "|", "");
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "Test");
+            await Clients.Group("Test").SendAsync("Disconnected", Users["Test"], username);
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
