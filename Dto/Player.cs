30014bdb6428fbfd319cd63c77fe4993e0206a66
Amor Osmic <amorosmic@gmx.com>
2021-05-24 12:22:22 +0200

    Created player dto and now we can tell who the admin of the room is

---
+++ Dto/Player.cs
@@ -0,0 +1,9 @@
+namespace Dto
+{
+    class Player
+    {
+        public string Username { get; set; }
+        public int Points { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
--- Hub/ChatHub.cs
+++ Hub/ChatHub.cs
@@ -9,7 +9,7 @@ namespace Hubs
 {
     class ChatHub : Hub
     {
-        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+        private static Dictionary<string, Dictionary<string, Player>> Users = new Dictionary<string, Dictionary<string, Player>>();
 
         public async Task SendMessage(string username, string message)
         {
@@ -19,43 +19,70 @@ namespace Hubs
         // todo: generate random room ids for groups
         public override async Task OnConnectedAsync() 
         {
-            await Groups.AddToGroupAsync(Context.ConnectionId, "Test");
-
-            string users = "";
-            if (Users.ContainsKey("Test"))
-            {
-                users = Users["Test"];
-            }
-
             var username = Context.GetHttpContext().Request.Query["username"].ToString();
 
             if (username == "")
             {
                 Context.Abort();
+                return;
             }
 
-            Context.Items.Add("Username", username);
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Test");
 
-            users += username + "|";
-            Users["Test"] = users;
+            Player player = new Player
+            {
+                Username = username,
+                Points = 0,
+                IsAdmin = false
+            };
 
-            await Clients.Group("Test").SendAsync("Connected", users, username);
+            if (!Users.ContainsKey("Test")) 
+            {
+                Users["Test"] = new Dictionary<string, Player>();
+            }
+            
+            Users["Test"].Add(Context.ConnectionId, player);
+
+            if (Users["Test"].Count <= 1) 
+            {
+                player.IsAdmin = true;
+            }
+
+            List<Player> activePlayers = new List<Player>();
+
+            foreach (KeyValuePair<string, Player> kvp in Users["Test"])
+            {
+                activePlayers.Add(kvp.Value);
+            }
+
+            await Clients.Group("Test").SendAsync("Connected", activePlayers, username);
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception exception) 
         {
-            string username = Context.Items["Username"].ToString();
-            Users["Test"] = Users["Test"].Replace(username + "|", "");
+            string username = Users["Test"][Context.ConnectionId].Username;
+            Users["Test"].Remove(Context.ConnectionId);
             
+            List<Player> activePlayers = new List<Player>();
+
+            foreach (KeyValuePair<string, Player> kvp in Users["Test"])
+            {
+                activePlayers.Add(kvp.Value);
+            }
+
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, "Test");
-            await Clients.Group("Test").SendAsync("Disconnected", Users["Test"], username);
+            await Clients.Group("Test").SendAsync("Disconnected", activePlayers, username);
             await base.OnDisconnectedAsync(exception);
         }
 
         public async Task SendMove(Position position) 
         {
             await Clients.OthersInGroup("Test").SendAsync("RecieveMove", position);
+        }
+
+        public async Task StartGame() {
+            await Clients.OthersInGroup("Test").SendAsync("RecieveGameStarted");
         }
     }
 }
